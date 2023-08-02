68a14cc5ac57a16cf96526cbe64d6e3628f17ccf
ansh mehta <anshmehta630@gmail.com>
2023-08-02 17:18:32 +0000

    dataypes in js

---
+++ 01_basicjs/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";// treat all js code as newer version
+
+ // alert(3+3)  we are using node js not browser 
+ console.log(4+4) 
+
+  console.log("ansh")
+  let name="ansh mehta"
+  let age =20
+  let isLoggedin=true // boolean type 
+  // primitive data types 
+  // number 
+  // bigint
+  // string
+  // null-> standalone value 
+  // undefined 
+  // symbol ->unique 
+
+  // object 
+  console.log(typeof undefined);
+  console.log(typeof null); // object type

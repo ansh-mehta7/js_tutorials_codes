328d5554d9d166fcb5b206008efd4628fdf38242
ansh mehta <anshmehta630@gmail.com>
2023-08-02 17:19:01 +0000

    datatypes conversion

---
+++ 01_basicjs/03_conversionandoperations.js
@@ -0,0 +1,17 @@
+let score ="ansh"
+
+console.log(typeof score);
+console.log((typeof score)); // type number 
+
+//let valuerInNumber= Number(score)
+//console.log(typeof valuerInNumber); // type number 
+ 
+//console.log(valuerInNumber);  // NaN value is not a number 
+// "33"-> 33
+// "33abdjd"-> nan
+// "true"  =1 
+
+// let isLoggedin= 1
+let isLoggedin=""
+let boolenIsLoggedin= Boolean(isLoggedin)
+console.log(boolenIsLoggedin);

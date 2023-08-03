789b9babc31660c844c0c89322f0d6cb399e7a42
ansh mehta <anshmehta630@gmail.com>
2023-08-03 05:48:28 +0000

    datatype summary

--- 01_basicjs/02_datatypes.js
+++ 01_basicjs/02_datatypes.js
@@ -7,14 +7,43 @@
   let name="ansh mehta"
   let age =20
   let isLoggedin=true // boolean type 
-  // primitive data types 
+  // primitive data types  they are call by value in js 
   // number 
-  // bigint
+  // bigint  // for large dta or big value 
   // string
-  // null-> standalone value 
+  // null-> standalone value  
   // undefined 
   // symbol ->unique 
+  // boolean
 
-  // object 
+    //refrence (non  primitive)
+  // arrays ,objects,functions
+  
+
   console.log(typeof undefined);
   console.log(typeof null); // object type
+ let userEmail; // by deafault it will be undefined 
+
+ const id=Symbol('123')
+ const anotherid=Symbol('123')
+ console.log(id===anotherid);
+ // although the value is same but in symbols the returned data type is diffrent  in symbol 
+
+
+const bigNumber=848483837292093939n //bigint
+
+const heros=["ronaldo ,jordan ,schwarzennager"] // refrence type 
+
+// anything specified in curly braces is objects 
+let myobj={
+    name :"ansh mehta ",
+     age:20,
+}
+// functions in js csn also be given a variable value 
+ const myFunction= function(){
+    console.log("cristiano ronaldo ");
+ }
+
+console.log(typeof myFunction);// return function object 
+console.log(typeof heros); // return objects 
+// symbols will retun the symbol return type 
